77b8fa1a8d44ad0d6deeb1b6802d578049bd5404
Aleksandar <aleksandarkurta1988@gmail.com>
2025-05-14 20:29:30 +0200

    Feat: create models and migrations for Product and ProductRating

---
+++ store-backend/app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'external_id',
+        'title',
+        'price',
+        'description',
+        'category',
+        'image'
+    ];
+
+    public function rating()
+    {
+        return $this->hasOne(ProductRating::class);
+    }
+}
